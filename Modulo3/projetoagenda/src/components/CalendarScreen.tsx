97d6712856a1847ba9724fe6b797fc5a42143e72
gabrielolisilva <gabrielsilva2799@hotmail.com>
2023-03-24 15:45:32 -0300

    Days of calendar implement

--- Modulo3/projetoagenda/src/components/CalendarScreen.tsx
+++ Modulo3/projetoagenda/src/components/CalendarScreen.tsx
@@ -6,9 +6,51 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Button from "@mui/material/Button";
-import { Checkbox, FormControlLabel } from "@mui/material";
+import {
+  Avatar,
+  Checkbox,
+  FormControlLabel,
+  Icon,
+  IconButton,
+} from "@mui/material";
 
 const daysWeek: string[] = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+
+interface ICalendarCell {
+  date: string;
+}
+
+function generateCalendar(date: string): ICalendarCell[][] {
+  const weeks: ICalendarCell[][] = []; //an array with 4 array related to each week
+  const jsDate = new Date(`${date}T12:00:00`);
+  const currentMonth = jsDate.getMonth();
+
+  const currentDay = new Date(jsDate.valueOf()); //copying jsDate into this new variable
+  currentDay.setDate(1); //getting first day of month
+
+  const dayOfWeek = currentDay.getDay(); //getting with day of week it is (0 is sunday)
+  currentDay.setDate(1 - dayOfWeek); //if day of week is 0 it keeps being sunday, but if it's not he will subtract until find with day is sunday
+
+  do {
+    const week: ICalendarCell[] = [];
+    for (let i = 0; i < daysWeek.length; i++) {
+      const isoDate = `${currentDay.getFullYear()}-${(currentDay.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${currentDay.getDate().toString().padStart(2, "0")}`;
+      week.push({ date: isoDate });
+      currentDay.setDate(currentDay.getDate() + 1);
+    }
+    weeks.push(week);
+  } while (currentDay.getMonth() === currentMonth);
+
+  return weeks;
+}
+
+function getTodayDate() {
+  return "2021-06-17";
+}
+
+const weeks = generateCalendar(getTodayDate());
 
 const CalendarScreen = () => {
   return (
@@ -21,11 +63,33 @@ const CalendarScreen = () => {
         <h2>Agenda React</h2>
         <Button variant="contained">Novo Evento</Button>
 
-        <h3>Agendas</h3>
-        <FormControlLabel control={<Checkbox />} label="Pessoal" />
-        <FormControlLabel control={<Checkbox />} label="Trabalho" />
+        <Box marginTop="64px">
+          <h3>Agendas</h3>
+          <FormControlLabel control={<Checkbox />} label="Pessoal" />
+          <FormControlLabel control={<Checkbox />} label="Trabalho" />
+        </Box>
       </Box>
       <TableContainer component={"div"}>
+        <Box padding="8px 16px" display="flex" alignItems="center">
+          <Box>
+            <IconButton aria-label="Mês anterior">
+              <Icon>chevron_left</Icon>
+            </IconButton>
+            <IconButton aria-label="Próximo mês">
+              <Icon>chevron_right</Icon>
+            </IconButton>
+          </Box>
+
+          <Box flex="1" marginLeft="16px" component="strong">
+            Junho de 2021
+          </Box>
+
+          <IconButton aria-label="Ícone profile">
+            <Avatar>
+              <Icon>person</Icon>
+            </Avatar>
+          </IconButton>
+        </Box>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
@@ -37,23 +101,15 @@ const CalendarScreen = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              {daysWeek.map((day) => (
-                <TableCell align="center">X</TableCell>
-              ))}
-            </TableRow>
-
-            <TableRow>
-              {daysWeek.map((day) => (
-                <TableCell align="center">X</TableCell>
-              ))}
-            </TableRow>
-
-            <TableRow>
-              {daysWeek.map((day) => (
-                <TableCell align="center">X</TableCell>
-              ))}
-            </TableRow>
+            {weeks.map((week, i) => (
+              <TableRow key={i}>
+                {week.map((cell) => (
+                  <TableCell align="center" key={cell.date}>
+                    {cell.date}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
