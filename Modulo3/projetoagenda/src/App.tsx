9ee814e424382b6dd35137e3cc9e0a76d8fdc211
gabrielolisilva <gabrielsilva2799@hotmail.com>
2023-03-30 17:01:52 -0300

    Route remove

--- Modulo3/projetoagenda/src/App.tsx
+++ Modulo3/projetoagenda/src/App.tsx
@@ -2,21 +2,13 @@ import "./App.css";
 
 import CalendarScreen from "./components/CalendarScreen";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  Link,
-} from "react-router-dom";
-import { getTodayDate } from "./dateFunctions";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getUserEndPoint, IUser } from "./backend/backend";
 import LoginScreen from "./components/LoginScreen";
 
 function App() {
   const [user, setUser] = useState<IUser | null>(null);
-  const todayDate = getTodayDate().substring(0, 7);
 
   useEffect(() => {
     /*.then(setUser) the same as ()=> setUser(user) */
@@ -32,10 +24,6 @@ function App() {
       <div className="App">
         <Router>
           <Routes>
-            {/* <Route
-              path="*"
-              element={<Navigate to={`/calendar/${todayDate}`} />}
-            /> */}
             <Route
               path="/calendar/:date"
               element={<CalendarScreen user={user} onSignOut={onSignOut} />}
