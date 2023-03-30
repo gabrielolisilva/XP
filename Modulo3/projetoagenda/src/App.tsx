c5125dc4521f2f002134aa53d5e1b7ef4c9f19fe
gabrielolisilva <gabrielsilva2799@hotmail.com>
2023-03-30 15:39:57 -0300

    Login authentication

--- Modulo3/projetoagenda/src/App.tsx
+++ Modulo3/projetoagenda/src/App.tsx
@@ -10,23 +10,36 @@ import {
   Link,
 } from "react-router-dom";
 import { getTodayDate } from "./dateFunctions";
+import { useEffect, useState } from "react";
+import { getUserEndPoint, IUser } from "./backend/backend";
+import LoginScreen from "./components/LoginScreen";
 
 function App() {
+  const [user, setUser] = useState<IUser | null>(null);
   const todayDate = getTodayDate().substring(0, 7);
 
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          {/* <Route
-            path="*"
-            element={<Navigate to={`/calendar/${todayDate}`} />}
-          /> */}
-          <Route path="/calendar/:date" element={<CalendarScreen />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  useEffect(() => {
+    /*.then(setUser) the same as ()=> setUser(user) */
+    getUserEndPoint().then(setUser, () => setUser(null));
+  }, []);
+
+  if (user) {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            {/* <Route
+              path="*"
+              element={<Navigate to={`/calendar/${todayDate}`} />}
+            /> */}
+            <Route path="/calendar/:date" element={<CalendarScreen />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  } else {
+    return <LoginScreen onSignIn={setUser} />;
+  }
 }
 
 export default App;
--- Modulo3/projetoagenda/src/backend/backend.tsx
+++ Modulo3/projetoagenda/src/backend/backend.tsx
@@ -16,10 +16,23 @@ export interface IEvent extends IEditingEvent {
   id: number;
 }
 
-export async function getCalendarEndPoint(): Promise<ICalendar[]> {
-  return await fetch("http://localhost:3500/calendars").then((resp) => {
+export interface IUser {
+  name: string;
+  email: string;
+}
+
+function handleResponse(resp: Response) {
+  if (resp.ok) {
     return resp.json();
-  });
+  } else {
+    throw new Error(resp.statusText);
+  }
+}
+
+export async function getCalendarEndPoint(): Promise<ICalendar[]> {
+  return await fetch("http://localhost:3500/calendars", {
+    credentials: "include",
+  }).then(handleResponse);
 }
 
 export async function getEventsEndPoint(
@@ -27,44 +40,62 @@ export async function getEventsEndPoint(
   to: string
 ): Promise<IEvent[]> {
   return await fetch(
-    `http://localhost:3500/events?date_gte=${from}&date_lte=${to}&_sort=date,time`
-  ).then((resp) => {
-    return resp.json();
-  });
+    `http://localhost:3500/events?date_gte=${from}&date_lte=${to}&_sort=date,time`,
+    {
+      credentials: "include",
+    }
+  ).then(handleResponse);
 }
 
 export async function createEventEndPoint(
   event: IEditingEvent
 ): Promise<IEvent> {
   return await fetch(`http://localhost:3500/events`, {
+    credentials: "include",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(event),
-  }).then((resp) => {
-    return resp.json();
-  });
+  }).then(handleResponse);
 }
 
 export async function updateEventEndPoint(
   event: IEditingEvent
 ): Promise<IEvent> {
   return await fetch(`http://localhost:3500/events/${event.id}`, {
+    credentials: "include",
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(event),
-  }).then((resp) => {
-    return resp.json();
-  });
+  }).then(handleResponse);
 }
 
 export async function deleteEventEndPoint(eventId: number): Promise<void> {
   return await fetch(`http://localhost:3500/events/${eventId}`, {
+    credentials: "include",
     method: "DELETE",
-  }).then((resp) => {
-    return resp.json();
-  });
+  }).then(handleResponse);
+}
+
+export async function getUserEndPoint(): Promise<IUser> {
+  return await fetch(`http://localhost:3500/auth/user`, {
+    credentials: "include",
+  }).then(handleResponse);
+}
+
+export async function signInEndPoint(
+  email: string,
+  password: string
+): Promise<IUser> {
+  return await fetch(`http://localhost:3500/auth/login`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(handleResponse);
 }
