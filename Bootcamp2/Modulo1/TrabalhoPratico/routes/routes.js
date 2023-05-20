fd8833391833b936905bd315c19c38ae44a9a6f6
Gabriel Silva <gabrielsilva2799@hotmail.com>
2023-05-20 12:54:24 -0300

    Routes created

--- Bootcamp2/Modulo1/TrabalhoPratico/routes/routes.js
+++ Bootcamp2/Modulo1/TrabalhoPratico/routes/routes.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 const router = express.Router();
-import { promises as fs, readFile } from "fs";
+import { promises as fs } from "fs";
+import _ from "lodash";
 
 router.get("/marcas/maisModelos", async (req, res) => {
   const data = await fs.readFile("./data/car-list.json");
@@ -14,6 +15,50 @@ router.get("/marcas/maisModelos", async (req, res) => {
     }
   });
   res.send(jsonData[indice]);
+});
+
+router.get("/marcas/menosModelos", async (req, res) => {
+  const data = await fs.readFile("./data/car-list.json");
+  const jsonData = JSON.parse(data);
+  let smallerValue = jsonData[0].models.length;
+  let indice = 0;
+
+  jsonData.map((obj, index) => {
+    if (obj.models.length < smallerValue) {
+      smallerValue = obj.models.length;
+      indice = index;
+    }
+  });
+  res.send(jsonData[indice]);
+});
+
+router.get("/marcas/listaMaisModelos/:count", async (req, res) => {
+  const { count } = req.params;
+  const data = await fs.readFile("./data/car-list.json");
+  const jsonData = JSON.parse(data);
+
+  const orderedArray = _.orderBy(jsonData, ["models.length"], ["desc"]);
+  const arrayRange = orderedArray.slice(0, count);
+  res.send(arrayRange);
+});
+
+router.get("/marcas/listaMenosModelos/:count", async (req, res) => {
+  const { count } = req.params;
+  const data = await fs.readFile("./data/car-list.json");
+  const jsonData = JSON.parse(data);
+
+  const orderedArray = _.orderBy(jsonData, ["models.length"], ["asc"]);
+  const arrayRange = orderedArray.slice(0, count);
+  res.send(arrayRange);
+});
+
+router.get("/marcas/listaModelos/:marca", async (req, res) => {
+  const { marca } = req.params;
+  const data = await fs.readFile("./data/car-list.json");
+  const jsonData = JSON.parse(data);
+
+  const especificBrand = _.find(jsonData, { brand: marca });
+  res.send(especificBrand);
 });
 
 export default router;
