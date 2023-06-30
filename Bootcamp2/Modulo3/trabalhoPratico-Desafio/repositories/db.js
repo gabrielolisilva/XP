f7a4706bbd2cf4536126155c1fa6be4a5be48d67
Gabriel Silva <gabrielsilva2799@hotmail.com>
2023-06-30 16:18:00 -0300

    Atualizar o db.js

--- Bootcamp2/Modulo3/trabalhoPratico-Desafio/repositories/db.js
+++ Bootcamp2/Modulo3/trabalhoPratico-Desafio/repositories/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-  "postgres://qrdtnxhs:e9xOVlTG0ok_5U3jgEnAffpBObe08l9b@silly.db.elephantsql.com/qrdtnxhs",
+  "",
   {
     dialect: "postgres",
     define: {
