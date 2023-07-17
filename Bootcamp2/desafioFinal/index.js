f9ea0117225dd1c325d572be305bcc39224d438c
Gabriel Silva <gabrielsilva2799@hotmail.com>
2023-07-17 19:27:39 -0300

    data insertion

--- Bootcamp2/desafioFinal/index.js
+++ Bootcamp2/desafioFinal/index.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-/* app.use("/proprietario", proprietariosRouter);
-app.use("/animal", animaisRouter);
-app.use("/servico", servicosRouter); */
+app.use("/vendas", vendasRouter);
+app.use("/clientes", clientesRouter);
+app.use("/livros", livrosRouter);
+app.use("/autores", autoresRouter);
 
 app.listen(3000, () => {
   console.log(`API RUNNING PORT 3000`);
